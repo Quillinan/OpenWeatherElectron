2506b6c8c33bb942b2ef5c6b2838346e4d55bfba
Quillinan <milton.quillinan@gmail.com>
2024-09-06 05:06:24 -0300

    chore: add csp and remove console.log

--- src/renderer/src/services/getForecast.tsx
+++ src/renderer/src/services/getForecast.tsx
@@ -3,10 +3,8 @@ import { api, apiKey, apiUrl } from "./api";
 
 export const getForecast = async (cityName: string): Promise<GraphicInfo> => {
   const url = `${apiUrl}/forecast?q=${encodeURIComponent(
-    cityName
+    cityName,
   )}&lang=pt_br&appid=${apiKey}&units=metric&ctn=40`;
-
-  console.log(apiKey);
 
   return api(url);
 };
