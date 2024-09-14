f97232042938d7231fbfbebb5042838e92b741a7
Quillinan <milton.quillinan@gmail.com>
2024-09-14 20:37:16 -0300

    style: fix exe icon

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,7 @@
 import { app, shell, BrowserWindow, ipcMain } from "electron";
 import { join } from "path";
 import { electronApp, optimizer, is } from "@electron-toolkit/utils";
-import icon from "../../resources/sun.ico?asset";
+import icon from "../../resources/icon.png?asset";
 
 function createWindow(): void {
   // Create the browser window.
@@ -11,7 +11,7 @@ function createWindow(): void {
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === "linux" ? { icon } : {}),
-    icon: join(__dirname, "../../resources/sun.ico"),
+    icon: join(__dirname, "../../resources/icon.ico"),
     webPreferences: {
       preload: join(__dirname, "../preload/index.js"),
       sandbox: false,
